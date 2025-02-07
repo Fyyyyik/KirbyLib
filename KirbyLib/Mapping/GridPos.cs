ed3d824600e7d9f73ac226ddeb46709363f88af5
firubii <fruitymage@gmail.com>
2025-02-06 19:09:56 -0600

    More GridPos documentation, and double support

--- KirbyLib/Mapping/GridPos.cs
+++ KirbyLib/Mapping/GridPos.cs
@@ -7,22 +7,33 @@ using System.Threading.Tasks;
 namespace KirbyLib.Mapping
 {
     /// <summary>
-    /// A representation of an object position within a map.
+    /// A fixed-point representation of an object position within a map.<br/><br/>
+    /// Its raw data is a 28-bit whole number and 4-bit decimal, as a 32-bit integer.
     /// </summary>
     public struct GridPos
     {
         private uint _value;
 
+        /// <summary>
+        /// Constructs a GridPos using a raw value as stored in level data.
+        /// </summary>
         public GridPos(uint rawPos)
         {
             _value = rawPos;
         }
 
-        public GridPos(uint whole, byte _decimal)
+        /// <summary>
+        /// Constructs a GridPos using a whole number and a given decimal value between 0 and 16.
+        /// </summary>
+        public GridPos(int whole, byte _decimal)
         {
-            _value = ((uint)(whole << 4) | (uint)(_decimal & 0xF));
+            _value = (uint)(whole << 4) | (uint)(_decimal & 0xF);
         }
 
+        /// <summary>
+        /// Constructs a GridPos using a floating-point value.<br/>
+        /// The value will be truncated to 16 possible decimal values.
+        /// </summary>
         public GridPos(float pos)
         {
             uint wholePart = (uint)Math.Floor(pos);
@@ -30,24 +41,97 @@ namespace KirbyLib.Mapping
             _value = (wholePart << 4) | decimalPart;
         }
 
+        /// <summary>
+        /// Constructs a GridPos using a 64-bit floating-point value.<br/>
+        /// The value will be truncated to 16 possible decimal values.
+        /// </summary>
+        public GridPos(double pos)
+        {
+            uint wholePart = (uint)Math.Floor(pos);
+            uint decimalPart = (uint)Math.Floor((pos - wholePart) * 16d);
+            _value = (wholePart << 4) | decimalPart;
+        }
+
+        /// <summary>
+        /// Creates a GridPos using a raw value as stored in level data.
+        /// </summary>
+        public static GridPos FromRaw(uint rawPos)
+        {
+            return new GridPos(rawPos);
+        }
+
+        /// <summary>
+        /// Creates a GridPos using an integer value.<br/>
+        /// The GridPos will have a decimal place of 0.
+        /// </summary>
+        public static GridPos FromInt(int pos)
+        {
+            return new GridPos(pos, 0);
+        }
+
+        /// <summary>
+        /// Creates a GridPos using a floating-point value.<br/>
+        /// The value will be truncated to 16 possible decimal values.
+        /// </summary>
+        public static GridPos FromFloat(float pos)
+        {
+            return new GridPos(pos);
+        }
+
+        /// <summary>
+        /// Gets the whole number component of the fixed-point value.
+        /// </summary>
         public uint GetWholeNumber()
         {
             return (_value & 0xFFFFFFF0) >> 4;
         }
 
-        public uint GetDecimal()
+        /// <summary>
+        /// Gets the decimal component of the fixed-point value
+        /// </summary>
+        public byte GetDecimal()
         {
-            return _value & 0xF;
+            return (byte)(_value & 0xF);
         }
 
+        /// <summary>
+        /// Gets the raw, unsigned integer value
+        /// </summary>
         public uint GetRawValue()
         {
             return _value;
         }
 
+        /// <summary>
+        /// Gets the floating-point representation of the value
+        /// </summary>
         public float AsFloat()
         {
             return _value / 16f;
+        }
+
+        /// <summary>
+        /// Gets the 64-bit floating-point representation of the value
+        /// </summary>
+        public double AsDouble()
+        {
+            return _value / 16d;
+        }
+
+        /// <summary>
+        /// Gets the decimal component as a floating-point value
+        /// </summary>
+        public float GetDecimalFloat()
+        {
+            return (_value & 0xF) / 16f;
+        }
+
+        /// <summary>
+        /// Gets the decimal component as a 64-bit floating-point value
+        /// </summary>
+        public double GetDecimalDouble()
+        {
+            return (_value & 0xF) / 16d;
         }
 
         #region Casts
@@ -72,6 +156,16 @@ namespace KirbyLib.Mapping
             return new GridPos(m);
         }
 
+        public static implicit operator double(GridPos m)
+        {
+            return m.AsDouble();
+        }
+
+        public static implicit operator GridPos(double m)
+        {
+            return new GridPos(m);
+        }
+
         #endregion
 
         #region Operators
@@ -88,12 +182,12 @@ namespace KirbyLib.Mapping
 
         public static GridPos operator *(GridPos a, GridPos b)
         {
-            return new GridPos(a.AsFloat() * b.AsFloat());
+            return new GridPos(a.AsDouble() * b.AsDouble());
         }
 
         public static GridPos operator /(GridPos a, GridPos b)
         {
-            return new GridPos(a.AsFloat() / b.AsFloat());
+            return new GridPos(a.AsDouble() / b.AsDouble());
         }
 
         public static bool operator ==(GridPos a, GridPos b)
@@ -130,7 +224,7 @@ namespace KirbyLib.Mapping
 
         public override string ToString()
         {
-            return AsFloat().ToString();
+            return AsDouble().ToString();
         }
 
         public override bool Equals(object obj)
